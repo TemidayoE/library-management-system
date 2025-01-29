130b1212c6e1c3835fc10632a06d8c56e1cfe1de
Temidayo Ejide <temidayoejide1@gmail.com>
2025-01-29 10:12:24 +0100

    Initial Commits

--- lib_mgt/settings.py
+++ lib_mgt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9lqrwwg+z#miig8!8)wsjcyo-&3l!2cd5f6zl2h-wr(1^s+wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
