b4f6a8c6544aa63abf6bc57d869a8bf0951bd02a
Temidayo Ejide <temidayoejide1@gmail.com>
2025-01-29 10:22:25 +0100

    New Commits

--- lib_app/urls.py
+++ lib_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from lib_app.views import BookListCreateView, BookRetrievalUpdateDeleteView
 
 urlpatterns = [
+    path('', lambda request: HttpResponse('Welcome to the Library Management System API!')),
     path('v1/books/', BookListCreateView.as_view(), name = 'create-book'),
     path('v1/books/<pk>/', BookRetrievalUpdateDeleteView.as_view(), name = 'book-details'),
 ]
