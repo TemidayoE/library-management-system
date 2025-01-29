89cfb988f0c438dbe25d313ef48586b57cfd8d07
Temidayo Ejide <temidayoejide1@gmail.com>
2025-01-29 10:25:01 +0100

    New Commit

--- lib_mgt/urls.py
+++ lib_mgt/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse('Welcome to the Library Management System API!')),
     path('api/', include('lib_app.urls'))
 ]
